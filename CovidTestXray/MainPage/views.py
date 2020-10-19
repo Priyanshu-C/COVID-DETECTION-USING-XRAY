20889b18a09c80bbf1d2db9c9be03f044ab22523
Priyanshu-C <63398163+Priyanshu-C@users.noreply.github.com>
2020-10-20 02:36:01 +0530

    Implemented the Model trying to injext and classify in the flask server side

--- CovidTestXray/MainPage/views.py
+++ CovidTestXray/MainPage/views.py
@@ -1,7 +1,34 @@
-from flask import render_template,request,Blueprint
-
+from flask import render_template,request,Blueprint,redirect
+from keras.models import load_model
+from cv2 import cv2
+import numpy as np
+import os
 core = Blueprint('core',__name__)
 
 @core.route('/')
-def index():
-    return render_template('index.html')
+def index():       
+    return render_template('index.html')                     
+
+@core.route('/', methods = ["POST"])
+def ModelTest():
+    MODEL = load_model("VGGModel")
+    if request.method == "POST":
+
+        X = request.files('imagefile')
+        return render_template('covid.html',X=X)
+
+        # image = request.files('imagefile')
+        # image = cv2.imread(image)
+        # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # image = image/255.0
+        # image = cv2.resize(image, (512, 512))
+        # image = image.reshape(-1, 512, 512, 3)
+
+        # TAG = ["COVID","NORMAL"]
+        # X = MODEL.predict(image)
+        # if TAG[np.argmax(X)] == "COVID":
+        #     return render_template('covid.html')
+        # else:
+        #     return render_template('normal.html')
+    
+
